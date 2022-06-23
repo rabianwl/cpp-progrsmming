8609ec273f43ac67ba61b70b2611c8f55dd6453a
rabianwl <00rabia.pk@gmail.com>
2022-06-23 16:18:06 +0500

    cpp file added

---
+++ capter no 2.cpp
@@ -0,0 +1,257 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//program 1	
+
+/*cout <<"programming is a great fun!";*/	
+//program 2
+
+/*cout << "programming is" <<"great fun";*/
+//program 3
+
+/*cout <<"programming is ";
+cout <<"great fun!";*/
+//program 4
+
+/*cout <<"the following items were top sellers ";
+cout <<"during the month of june ";
+cout <<"computers game ";
+cout <<"aspirin ";*/
+//program 5
+
+/*cout <<"the following items were top sellers" << endl;
+cout <<"during the month of june : " << endl ;
+cout <<"computers game" << endl;
+cout <<"coffee"<< endl;
+cout <<"aspirin" <<endl ;*/
+//program 6
+
+/*cout <<"the following items were top sellers\n";
+cout <<"during the month of june :/n";
+cout <<"computer games \n coffee";
+cout <<"\n aspirin\n";*/
+//program 7
+
+/*int number;
+number = 5;
+cout <<"the value in number is" << number <<endl;*/
+//program 8
+
+/*int number;
+number=5;
+cout <<"the value of number is " <<"number";*/
+//program 9
+
+/*int apples;
+apples=20;
+cout << "today we sold " << apples << " bushels of apples";*/
+//program 10
+
+/*int checking;
+unsigned int miles;
+long days;
+checking = -20;
+miles = 4276;
+days = 189000;
+cout <<"we have made a long journey of "<< miles;
+cout <<"miles.\n";
+cout <<"our checking account balance is "<< checking;
+cout <<"\nAbout " << days << " days ago columns ";
+cout <<"stood o this spot.\n";*/
+//program 11
+
+/*int floors, rooms, suites;
+floors = 15;
+rooms = 300;
+suites = 30;
+cout <<"the grand hotel has " << floors << " floors\n";
+cout <<"with " << rooms <<"  rooms and " << suites;
+cout <<" suites.\n";*/
+//program 12
+
+/*char letter;
+letter = 'A';
+cout << letter << endl;
+letter = 'B';
+cout << letter << endl;*/
+//program 13
+
+/*char letter;
+letter=65;
+cout << letter <<endl;
+letter=66;
+cout << letter <<endl;*/
+//proogram 14
+
+/*char letter;
+letter = 'A';
+cout << letter <<'\n';
+letter = 'B';
+cout << letter << '\n';*/
+//program 15
+
+/*string movieTitle;
+movieTitle = "wheels of fury";
+cout << "my favourite movie is " << movieTitle << endl;*/
+//program 16
+
+/*float distance;
+double mass;
+distance = 1.495979E11;
+mass = 1.989E30;
+cout << "the sun is " << distance << " meters away.\n";
+cout << "the sun\'s mass is " << mass << " kilogram.\n";*/
+//program 17
+
+/*bool boolvalue;
+boolvalue=true;
+cout << boolvalue <<endl;
+boolvalue=false;
+cout << boolvalue <<endl;*/
+//program 18
+
+/*long double apple;
+cout << "the size of integer is " << sizeof(int);
+cout << " bytes.\n";
+cout << "the bytes of long integer is " << sizeof(long);
+cout << "bytes.\n";
+cout << "an apple can be eaten in " << sizeof (apple);
+cout << " bytes!\n";*/
+//program 19
+
+/*int month=2 , days=28 ;
+cout << "month " << month << " has " << days << " days.\n";*/
+//program 20
+
+/*cout << value; // ERROR! value not defined yet !
+int value = 100;*/
+//proogram 21
+
+/*double regularWages,       //to hold reglar wages
+ basePayRate = 18.25,      //base pay rate
+ regularHours = 40.0,      //hours worked less overtime
+ overtimeWages,            //to hold over time wages
+ overtimePayRate = 27.78,  //overtime pay rate 
+ overtimeHours = 10,       //overtime hours worked
+ totalWages;               //to hold total wages
+// caluculate the regular wages
+ regularWages = basePayRate * regularHours;
+// caluculate the over time wages 
+ overtimeWages = overtimePayRate * overtimeHours;
+//caluculate the total wages
+ totalWages = regularWages + overtimeWages;
+//display the total wages.
+ cout << "wages for this week are $ "  << totalWages << endl;*/
+ //program 22
+ 
+/*// variable to hold the monthly pay and the
+// amount of contribution.
+double monthlypay = 6000.0, contribution;
+//caluculate and display a 5% contribution 
+contribution = monthlypay * 0.05;
+cout << "5 percent is $" << contribution
+     << " per month.\n";
+//calculate and display a 10% contribution.
+contribution = monthlypay * 0.1;
+cout << "10 percent is $" <<contribution
+     << " per month.\n" ;*/
+//program 23
+
+/*// variable to hold the regular price, the
+// amount of a discount ,and the sale prise.
+double regularPrice = 59.95, discount, saleprice;
+//calculate the amount of a 20% discount.
+discount = regularPrice * 0.2;
+// calculate the sale price by subtracting the
+//discount from the regular price.
+saleprice = regularPrice - discount;
+//display the results.
+cout <<"regular price: $" << regularPrice << endl;
+cout <<"discount amount: $" << discount << endl;
+cout <<"sale price: $" << saleprice <<endl; */
+//program 24
+
+/*int number=12345;
+int rightmost=number%10;
+cout <<"the rightmost digit in "   
+     <<number <<" is "
+     <<rightmost <<endl;*/
+//program 25
+
+/*//the total seconds is 125.
+int totalseconds = 125;
+//variable for minutes and second 
+int minutes, seconds;
+//get the number of minutes
+minutes = totalseconds / 60;
+// get the remainning seconds 
+seconds = totalseconds % 60;
+// display results 
+cout <<"totalseconds " <<"seconds is equivalent to:\n";
+cout <<" minutes :" << minutes << endl;
+cout <<" seconds :" << seconds << endl;*/
+//program 26 
+
+/*// PROGRAM: PAYROLL.CPP
+// written by Herbert Dorfman 
+//this program caluculate company payroll
+//last modification: 8/20/2014
+double payRate;  //holds hourly pay rate
+double Hours;    //holds the hours worked
+int employNumber;//holds the employ number*/
+//program 27
+
+/*
+PROGRAM: PAYROLL.CPP
+written by Herbert Dorfmann
+this programm caluculate company payroll
+last modification: 8/20/2014
+
+double payRate;  //holds hourly pay rate
+double Hours;    //holds the hours worked
+int employNumber;//holds the employ number*/
+//program 28
+
+
+//constants
+
+/*const double PI = 3.141659;
+const double DIAMETER = 10.0;
+// variable to hold the circumference
+double circumference;
+// caluculate the circumference.
+
+circumference = PI * DIAMETER;
+//display the circumference.
+//cout << " the circumference is : " << circumference << endl;
+cout<< "The circumference is: " << circumference << endl;*/
+
+/*//program 29
+double shares=220.0;
+double avgprice=14.69;
+cout<<"three were "<<shares<<" shares sold at $" <<" per share.\n";*/
+/*//program 30
+
+double shares = 220.0;
+double avgprice = 14.67;
+cout << " there were  " << shares << "shares sold at $";
+cout << " avgprice " << " per share.\n";*/
+
+
+
+
+
+
+
+
+
+
+    
+     
+	     
+ 
+ 
+                                                                                                                                                                                                                                                                                
+}
