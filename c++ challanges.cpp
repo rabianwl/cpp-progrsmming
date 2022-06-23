1e8657dfaa19407001d73608c6f7904154d5f65f
rabianwl <00rabia.pk@gmail.com>
2022-06-23 16:31:13 +0500

    added new file

---
+++ c++ challanges.cpp
@@ -0,0 +1,206 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//chalange#1
+ 
+/*int A,B,TOTAL;
+A = 50 ;
+cout <<" value of A is : " << A << endl ;
+B = 100;
+cout <<" value of B is : " << B << endl ;
+TOTAL = A+B;
+cout <<" TOTAL of A and B is : " << TOTAL << endl ;*/
+
+//chalange#2
+
+/*double total_sale_percentage=58,sales_of_year=8.6E6,east_cost,divide;
+divide=total_sale_percentage/100;
+east_cost=divide*sales_of_year;
+cout << "the totle east cost " << east_cost;*/
+
+//chalange#3
+
+/*double sales_rate=95,
+       state_sales_tax,
+       state_tax,
+       country_sales_tax,
+       country_tax,
+       total_tax;
+//state_sales_tax=4/100;
+state_tax=0.04*sales_rate;
+cout << "the state sales tax is : " << state_tax <<endl;
+//country_sales_tax=2/100;
+country_tax=0.02*sales_rate;
+cout << "the country sales tax is : " << country_tax <<endl;
+total_tax=state_tax+country_tax;
+cout << "the total tax is : " << total_tax << endl;*/
+
+//chalange#4
+ 
+/*double meal_charge,tax,tip,total;
+meal_charge=88.67;
+tax=0.0675*88.67;  //tax=6.75/100
+tip=(meal_charge+tax)*0.2 ; //tip=20/100
+total=meal_charge+tax+tip;
+cout << "meal charge is : " << meal_charge << endl;
+cout << "the tax on meal charge is : " << tax << endl;
+cout << "the tip on meal charge is : " << tip << endl;
+cout << "the total bill is : " << total << endl;*/
+
+//chalange#5
+
+/*double A=28,B=32,C=37,D=24,E=33,sum,Average;
+sum=A+B+C+D+E;
+Average=sum/5;
+cout <<"the average of numbers is : " << Average ;*/
+
+//challenge no 6
+
+/*double pay_amount=2200,
+       no_of_period=26,
+	   total_amount;
+total_amount=pay_amount*no_of_period;
+cout << "The total amount pay in a year is : " << total_amount << endl;*/
+
+//challenge no 7
+
+/*double levelpYear=1.5,
+       levelp5Year,
+       levelp7Year,
+       levelp10Year;
+levelp5Year=levelpYear*5;
+levelp7Year=levelpYear*7;
+levelp10Year=levelpYear*10;
+cout <<"The number of millimeters higher than the current level that the ocean’s level will be in 5 year is : " << levelp5Year<< endl;
+cout <<"The number of millimeters higher than the current level that the ocean’s level will be in 7 year is : " << levelp7Year<< endl;
+cout <<"The number of millimeters higher than the current level that the ocean’s level will be in 10 year is : " << levelp10Year<< endl; */
+
+//challenge no 8
+
+/*double item1=15.95,
+       item2=24.95,
+       item3=6.95,
+       item4=12.95,
+       item5=3.95,
+       tax=7,
+       subtotal,
+       amount,
+       total;
+subtotal=item1+item2+item3+item4+item5;
+amount=(tax/100)*subtotal;
+total=amount+subtotal;
+cout << "The price of item 1 is : " << item1 << endl;
+cout << "The price of item 2 is : " << item2 << endl;
+cout << "The price of item 3 is : " << item3 << endl;
+cout << "The price of item 4 is : " << item4 << endl;
+cout << "The price of item 5 is : " << item5 << endl; 
+cout << "The subtotal price is : " << subtotal << endl;
+cout << "The amount of sales tax is : " << amount << endl;
+cout << "The total amount is : " << total << endl; */
+
+//challenge no 9
+  
+/*int a=sizeof(int),b=sizeof(char),c=sizeof(float),d=sizeof(double),total_size;
+cout << "memory of int is : " << a << "bytes" << endl; 
+cout << "memory of char is : " << b << "bytes" << endl; 
+cout << "memory of float is : " << c << "bytes" << endl; 
+cout << "memory of double is : " << d << "bytes" << endl; 
+total_size=a+b+c+d;
+cout <<" the total size of data types is : " << total_size << endl;*/
+
+//challenge no 10
+  
+/*double no_of_gallons=15,
+        distance_travel=375,
+        MGP;
+cout << "total number of gallons : " << no_of_gallons << endl;
+cout << "distance travel by car is : " << distance_travel << endl;
+MGP=no_of_gallons/distance_travel;
+cout << "the number of miles per second is : " << MGP << endl;*/
+
+//challange no 11
+
+/*double no_of_gallons=20,
+       t_average=23.5,
+       h_average=28.9,
+       distace_t,
+       distace_h;
+distace_t= no_of_gallons* t_average;
+distace_h= no_of_gallons*h_average ;
+cout <<"The distance travel by a car in town is : "  <<  distace_t << endl;
+cout <<"The distance travel by a car on highway is : "  << distace_h << endl;*/
+
+//challange no 12
+
+/*double one_acre=43560,
+       land=391876,
+       no_of_acre;
+no_of_acre=land/one_acre;
+cout << "No of acres in 391876 squre feet of land is : " << no_of_acre << endl;*/
+
+//challange no 13
+
+/*double profit=35,
+       cost=14.95,
+       selling_profit,
+       selling_price;
+selling_profit=(profit/100)*cost;
+selling_price=selling_profit+cost;
+cout << "The selling price of a circuit board is : " << selling_price << endl; */
+
+//challenge no 14
+
+/*cout << "Rabia Kousar" << endl
+     << "village Dhadiala,Narrowal," << endl
+     << "o333 3333333" << endl
+     << "computer science" << endl ;*/
+     
+//challenge no 15
+
+/* 
+cout << "   *" << endl;
+cout << "  ***" << endl;
+cout << " *****" << endl;
+cout << "********" << endl;*/
+
+//challenge no 16
+
+/*    
+cout << "   *" << endl;
+cout << "  ***" << endl;
+cout << " *****" << endl;
+cout << "********" << endl;   
+cout << " *****" << endl;    
+cout << "  ***" << endl;    
+cout << "   *" << endl;  */  
+
+//challenge no 16
+
+/*double nStrocks=750,
+       price=35,
+       pCommission=2,
+       pStrock,
+       aCommission,
+       tAmount;
+pStrock=nStrocks/price;
+aCommission=(pCommission/100)*nStrocks;
+tAmount=aCommission+nStrocks;
+cout <<"the amount pay for a strock is : " << pStrock << endl;
+cout <<"the amount of commission is : " << aCommission << endl;  
+cout <<"the total amount paid is : " << tAmount<< endl; */
+
+//challenge no 18
+
+/*double tcustomers=16500,
+       pWpurchaser=15,
+	   cFpurchaser=58,
+	   PerWEEKpurchaser,
+	   citrusFpurchaser;
+PerWEEKpurchaser=(pWpurchaser/100)*tcustomers;
+citrusFpurchaser=(cFpurchaser/100)*PerWEEKpurchaser;
+cout << "Number of customers purchase one or more drink in one week : " <<PerWEEKpurchaser<< endl;
+cout << "Number of customers in the survey who prefer citrus-flavored drink : " << citrusFpurchaser << endl; */
+    
+    
+}
